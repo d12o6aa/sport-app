7fa0b6cd9918ef42676d28c7e886e76cd09fbbff
d12o6aa <doaakarem798@gmail.com>
2026-01-14 14:22:59 +0200

    refactor: streamline run.py by consolidating imports and removing redundant code

--- run.py
+++ run.py
@@ -1,15 +1,11 @@
-# run.py
 import eventlet
 eventlet.monkey_patch()
 
 from app import create_app
-from app.extensions import socketio
-from app.extensions import db, migrate
+from app.extensions import socketio, db, migrate
 
 app = create_app()
-app = create_app()
 migrate.init_app(app, db)
-if __name__ == '__main__':
-    app = create_app()
-    
+
+if __name__ == "__main__":
     socketio.run(app, debug=True, port=5000)
